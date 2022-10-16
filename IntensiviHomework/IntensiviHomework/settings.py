8c516f0631609b07512854de873cfc7dd28ed332
liltousin <sergeypletnev2004@gmail.com>
2022-10-16 23:14:38 +0300

    refactor manage.py

--- IntensiviHomework/IntensiviHomework/settings.py
+++ IntensiviHomework/IntensiviHomework/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(1zd&pxb$8ero6=v=9n5m(3cbfd6cs)d-9r6rkc!c^r3uf##p@'
+SECRET_KEY = 'django-insecure-(1zd&pxb$8erЭo6=v=9n5m(3cbfd6cs)d-9r6rkc!c^r3uf##p@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- IntensiviHomework/manage.py
+++ IntensiviHomework/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IntensiviHomework.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'IntensiviHomework.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
