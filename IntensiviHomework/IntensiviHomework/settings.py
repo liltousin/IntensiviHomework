976836aac6240d47254dba3baf4b510ecaec57aa
liltousin <sergeypletnev2004@gmail.com>
2022-10-17 00:00:03 +0300

    remove apps folders in project main folder

--- IntensiviHomework/IntensiviHomework/settings.py
+++ IntensiviHomework/IntensiviHomework/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.apps.AboutConfig',
+    'catalog.apps.CatalogConfig',
+    'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
