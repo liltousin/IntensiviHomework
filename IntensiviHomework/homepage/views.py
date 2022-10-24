7ae43aa9fc39e3b44f9b51e17054327705a71da3
liltousin <sergeypletnev2004@gmail.com>
2022-10-25 00:27:57 +0300

    I'm teapot status

--- IntensiviHomework/homepage/views.py
+++ IntensiviHomework/homepage/views.py
@@ -2,8 +2,10 @@
 
 # Create your views here.
 
+from http import HTTPStatus
+
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Главная')
+    return HttpResponse('Главная', status=HTTPStatus.IM_A_TEAPOT)
