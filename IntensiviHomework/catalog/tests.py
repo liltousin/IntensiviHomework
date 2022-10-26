c33e6fc689c37063cd5e82531e606b29a59a2206
liltousin <sergeypletnev2004@gmail.com>
2022-10-26 23:27:28 +0300

    add catalog tests

--- IntensiviHomework/catalog/tests.py
+++ IntensiviHomework/catalog/tests.py
@@ -37,3 +37,51 @@ class StaticURLTests(TestCase):
 
         response = Client().get('/catalog/qwer/')
         self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/0')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/0322')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/-1')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/-1231')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/-1qwe')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/1231qwe')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qwee-123')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qwer')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  er')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  er/')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  123er/')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  123er')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  123/')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/qw  123')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/q-w  -123er/')
+        self.assertEqual(response.status_code, 404)
+
+        response = Client().get('/catalog/q-w  -123er')
+        self.assertEqual(response.status_code, 404)
